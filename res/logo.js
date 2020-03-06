2fadcc795654ea0109f5b7d78663a1990d2b744c
subkultur <subkultur@protonmail.com>
2020-03-06 13:04:06 +0100

    change index: move dots, small animation

--- res/logo.js
+++ res/logo.js
@@ -5,4 +5,57 @@ document.addEventListener('theme-change', (e) => {
     )
 });
 
+// function translate( elem, x, y ) {
+//     var left = parseInt( css( elem, 'left' ), 10 ),
+//         top = parseInt( css( elem, 'top' ), 10 ),
+//         dx = left - x,
+//         dy = top - y,
+//         i = 1,
+//         count = 20,
+//         delay = 20;
 
+//     function loop() {
+//         if ( i >= count ) { return; }
+//         i += 1;
+//         elem.style.left = ( left - ( dx * i / count ) ).toFixed( 0 ) + 'px';
+//         elem.style.top = ( top - ( dy * i / count ) ).toFixed( 0 ) + 'px';
+//         setTimeout( loop, delay );
+//     }
+
+//     loop();
+// }
+//
+// function translateD( elem, dx, dy ) {
+//     var //left = parseInt( css( elem, 'left' ), 10 ),
+//         //top = parseInt( css( elem, 'top' ), 10 ),
+//         //dx = left - x,
+//         //dy = top - y,
+//         left = elem.cx.value,
+//         top = elem.cy.value,
+//         i = 1,
+//         count = 20,
+//         delay = 20;
+
+//     function loop() {
+//         if ( i >= count ) { return; }
+//         i += 1;
+//         //elem.style.left = ( left - ( dx * i / count ) ).toFixed( 0 ) + 'px';
+//         //elem.style.top = ( top - ( dy * i / count ) ).toFixed( 0 ) + 'px';
+//         elem.cx.
+//         setTimeout( loop, delay );
+//     }
+
+//     loop();
+// }
+
+// function css( element, property ) {
+//     return window.getComputedStyle( element, null ).getPropertyValue( property );
+// }
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     debugger;
+//     document.querySelectorAll(".l2nd").forEach((e) => {
+//         translate(e, css(e, 'left') + 120, css(e, 'top'));
+//     });
+
+// });
