f2d9538db546ce47bec3e7711df1cff47f33abd7
subkultur <subkultur@users.noreply.github.com>
2021-09-28 20:00:14 +0000

    Publishing Site https://subkultur.github.io at 8b98450bb4b8dc3ec2f39d2d163c591e75bc35de on Tue Sep 28 20:00:14 UTC 2021

--- res/logo.js
+++ res/logo.js
@@ -1,7 +1,7 @@
 document.addEventListener('theme-change', (e) => {
     const dark = e.detail === 'dark';
     document.querySelectorAll(".big, .small, .lcircle").forEach((e) => 
-        dark ? e.classList.remove('light') : e.classList.add('light')
+        e.classList.toggle('light', !dark)
     )
 });
 
